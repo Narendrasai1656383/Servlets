7978affcd423890464373de460613369825dd2f1
next <next@next-9804>
2025-02-28 17:01:09 +0530

    commit

--- DemoApp/src/main/java/com/telusko/AddServlet.java
+++ DemoApp/src/main/java/com/telusko/AddServlet.java
@@ -13,14 +13,14 @@ import javax.servlet.http.HttpSession;
 public class AddServlet extends HttpServlet{
 	public void doGet(HttpServletRequest req,HttpServletResponse res) throws IOException, ServletException {
 		
-		int i=Integer.parseInt(req.getParameter("num1"));
-		int j=Integer.parseInt(req.getParameter("num2"));
-		int k=i+j;
-		Cookie cookie=new Cookie("k",k+"");
-		res.addCookie(cookie);
-//		HttpSession ses=req.getSession(); 
-//		ses.setAttribute("k",k);
-		res.sendRedirect("square");
+//		int i=Integer.parseInt(req.getParameter("num1"));
+//		int j=Integer.parseInt(req.getParameter("num2"));
+//		int k=i+j;
+//		Cookie cookie=new Cookie("k",k+"");
+//		res.addCookie(cookie);
+////		HttpSession ses=req.getSession(); 
+////		ses.setAttribute("k",k);
+//		res.sendRedirect("square");
 //		req.setAttribute("k",k);
 //		RequestDispatcher rd=req.getRequestDispatcher("/square");
 //		rd.forward(req, res);
